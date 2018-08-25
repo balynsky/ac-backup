4fcde8a781fd175aff6ea1308dbaf36d36d13eaa
Sergey Balynsky <sb@spinbackup.com>
2018-08-25 21:05:58 +0300

    Initial commit

---
+++ backend/backup-service/src/main/java/com/balynsky/ac/backup/BackupApplication.java
@@ -0,0 +1,12 @@
+package com.balynsky.ac.backup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupApplication.class, args);
+	}
+}
